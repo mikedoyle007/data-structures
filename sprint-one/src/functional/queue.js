00dabe46b780752e8a5c8cc1c8e5c9bedaa8313c
Mike Doyle <mikedoyle007@gmail.com>
2017-06-28 12:00:09 -0700

    Remove comments from Queue - functional pattern
    Continues to pass tests.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,39 +1,28 @@
 var Queue = function() {
   var someInstance = {};
 
-  // Use an object with numeric keys to store values
   var storage = {};
   var size = 0;
   var head = 0;
   var tail = 0;
 
-  // Implement the methods below
-
   someInstance.enqueue = function(value) {
-    // add to end of line
     storage[tail] = value;
-    // increment the tail indicator
     tail++;
-    // increment size
     size++;
 
   };
 
   someInstance.dequeue = function() {
-    // remove from front of line
     var item = storage[head];
-    // increment head
     head++;
-    // decrement size
     size--;
-    // return item that was dequeued
     return item;
   };
 
   someInstance.size = function() {
     if (size < 0) {
       size = 0;
-      // also reset head and tail
       head = 0;
       tail = 0;
     }
