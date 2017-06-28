d9618fdb1b321b95c888fa9f6c4fcfdc7dbb36b0
Mike Doyle <mikedoyle007@gmail.com>
2017-06-28 16:25:14 -0700

    Change how the size is calculated
    Simplified by removing the sizeCount var because size was equal to
    tail var - head var.

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,7 +1,5 @@
 var Queue = function() {
   var newQueue = Object.create(queueMethods);
-
-  newQueue.sizeCount = 0;
   newQueue.storage = {};
   newQueue.head = 0;
   newQueue.tail = 0;
@@ -13,21 +11,20 @@ var queueMethods = {};
 
 queueMethods.enqueue = function(value) {
   this.storage[this.tail] = value;
-  this.sizeCount++;
   this.tail++;
 };
 
 queueMethods.dequeue = function() {
   var item = this.storage[this.head];
   this.head++;
-  this.sizeCount--;
   return item;
 };
 
 queueMethods.size = function() {
-  if (this.sizeCount < 0) {
-    this.sizeCount = 0;
+  if ((this.tail - this.head) < 0) {
+    this.head = 0;
+    this.tail = 0;
   }
-  return this.sizeCount;
+  return this.tail - this.head;
 };
 
