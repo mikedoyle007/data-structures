66597f7c8ba5678d8fe4bd4e0944c9fbcac3e846
Mike Doyle <mikedoyle007@gmail.com>
2017-06-28 16:18:08 -0700

    Change how size is calculated
    Simplified by eliminating the size variable since it was equal
    to tail variable - head variable.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -2,31 +2,26 @@ var Queue = function() {
   var someInstance = {};
 
   var storage = {};
-  var size = 0;
   var head = 0;
   var tail = 0;
 
   someInstance.enqueue = function(value) {
     storage[tail] = value;
     tail++;
-    size++;
-
   };
 
   someInstance.dequeue = function() {
     var item = storage[head];
     head++;
-    size--;
     return item;
   };
 
   someInstance.size = function() {
-    if (size < 0) {
-      size = 0;
+    if ((tail - head) < 0) {
       head = 0;
       tail = 0;
     }
-    return size;
+    return tail - head;
   };
 
   return someInstance;
