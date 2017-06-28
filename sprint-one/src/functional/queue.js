db3e97362c576d1a548c1d78e751915087ed1791
Mike Doyle <mikedoyle007@gmail.com>
2017-06-28 11:52:32 -0700

    Show first attempt at implementing Queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,13 +3,31 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
+  var head = 0;
+  var tail = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    // add to end of line
+    storage[tail] = value;
+    // increment the tail indicator
+    tail++;
+    // increment size
+    size++;
+
   };
 
   someInstance.dequeue = function() {
+    // remove from front of line
+    var item = storage[head];
+    // increment head
+    head++;
+    // decrement size
+    size--;
+    // return item that was dequeued
+    return item;
   };
 
   someInstance.size = function() {
