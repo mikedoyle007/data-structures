07071a54623fd23c2a1c3ca3fc1a744628b1d608
Mike Doyle <mikedoyle007@gmail.com>
2017-06-28 12:22:41 -0700

    Show basic outline of implementation so far
    Need to implement the function bodies still.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,25 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var instance = {};
+  var storage = {};
+  var size = 0;
+  var head = 0;
+  var tail = 0;
+  _.extend(instance, instance.queueMethods);
+
+  return instance;
+
 };
 
-var queueMethods = {};
+// var queueMethods = {};
+instance.queueMethods = {};
+
+instance.queueMethods.enqueue = function(value) {};
+
+instance.queueMethods.dequeue = function(value) {};
+
+instance.queueMethods.size = function(value) {};
 
 
