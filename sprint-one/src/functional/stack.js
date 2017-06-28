822c3ea4ffd21f0e381d1eee5ad9b813597fffaa
Mike Doyle <mikedoyle007@gmail.com>
2017-06-28 12:05:03 -0700

    Pass tests for Stack - functional pattern

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,24 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[size] = value;
+    size++;
   };
 
   someInstance.pop = function() {
+    size--;
+    return storage[size];
   };
 
   someInstance.size = function() {
+    if (size < 0) {
+      size = 0;
+    }
+    return size;
   };
 
   return someInstance;
