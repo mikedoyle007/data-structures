41594b67855294482b0c5d626b158057f515b669
Mike Doyle <mikedoyle007@gmail.com>
2017-06-29 10:42:43 -0700

    Pass tests for linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,33 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = new Node(value);
+    if (list.head === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      var oldTail = list.tail;
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
+    var oldHead = list.head;
+    list.head = list.head.next;
+    return oldHead.value;
   };
 
   list.contains = function(target) {
+    var currentVal = list.head;
+    while (currentVal !== null) {
+      if (currentVal.value === target) {
+        return true;
+      } else {
+        currentVal = currentVal.next
+      }
+    }
+    return false;
   };
 
   return list;
