01f6589e9bd5f1323fe40104d569def0fc322784
Mike Doyle <mikedoyle007@gmail.com>
2017-06-30 11:03:02 -0700

    Pass tests for set - add function

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,19 +1,18 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = item;
 };
 
-setPrototype.contains = function(item) {
-};
+setPrototype.contains = function(item) {};
 
-setPrototype.remove = function(item) {
-};
+setPrototype.remove = function(item) {};
 
 /*
  * Complexity: What is the time complexity of the above functions?
