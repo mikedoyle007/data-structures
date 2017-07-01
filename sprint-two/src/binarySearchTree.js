566ed0e7ab88c5ca59dcc87b71b7b12dc3c56cb4
Mike Doyle <mikedoyle007@gmail.com>
2017-06-30 20:32:09 -0700

    Create empty methods for BinarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,17 @@
 var BinarySearchTree = function(value) {
+  this._storage = {};
+  this._storage.value = value;
+  this._storage.right = null;
+  this._storage.left = null;
 };
 
+BinarySearchTree.prototype.left = function() {};
 
+BinarySearchTree.prototype.right = function() {};
+
+BinarySearchTree.prototype.insert = function() {};
+
+BinarySearchTree.prototype.insert = function() {};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
