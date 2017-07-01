246a21a379bab493a876c65dc4c95e281b415d43
Mike Doyle <mikedoyle007@gmail.com>
2017-06-30 20:35:14 -0700

    Replaced duplicate insert method with depthFirstLog method

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -13,7 +13,7 @@ BinarySearchTree.prototype.insert = function() {};
 
 BinarySearchTree.prototype.contains = function() {};
 
-BinarySearchTree.prototype.insert = function() {};
+BinarySearchTree.prototype.depthFirstLog = function() {};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
